1b4e3d66bd0701f4f9f06da66794fa76bedc91e0
Friedemann Sommer <social@friedemannsommer.com>
2023-08-25 03:10:52 +0200

    added documentation for `Options`, `StreamOptions`, and the summary types.

--- src/typings/file.ts
+++ src/typings/file.ts
@@ -1,30 +1,78 @@
 export interface SectionSummary {
+    /**
+     * The overall number of branches found and how often they were hit.
+     */
     branchSummary: Summary
+    /**
+     * List of branch entries, which contains one entry for each branch that was found.
+     */
     branches: BranchEntry[]
+    /**
+     * The overall number of functions found and how often they were hit.
+     */
     functionSummary: Summary
+    /**
+     * List of function entries, which contains one entry for each found function.
+     */
     functions: FunctionEntry[]
+    /**
+     * The overall number of found lines and how often they were hit.
+     */
     lineSummary: Summary
+    /**
+     * List of line entries, which contains one entry for each line that were found.
+     */
     lines: LineEntry[]
+    /**
+     * The section name (also referred to as "test name").
+     */
     name: string
+    /**
+     * The source file path.
+     */
     path: string
 }
 
 export interface Summary {
+    /**
+     * The number of executions for this branch, function, or line.
+     */
     hitCount: number
+    /**
+     * The number of branches, functions, or lines found.
+     */
     instrumented: number
 }
 
 export interface LineEntry {
+    /**
+     * Number of executions for this line.
+     */
     hitCount: number
+    /**
+     * The associated line number.
+     */
     lineNumber: number
 }
 
 export interface FunctionEntry extends LineEntry {
+    /**
+     * Function name
+     */
     name: string
 }
 
 export interface BranchEntry extends LineEntry {
+    /**
+     * The block number.
+     */
     blockNumber: number
+    /**
+     * The expression of the branch.
+     */
     expression: string
+    /**
+     * Possibly a `catch` or otherwise exception related branch.
+     */
     isException: boolean
 }
--- src/typings/options.ts
+++ src/typings/options.ts
@@ -3,14 +3,30 @@ import type { Readable, TransformOptions } from 'node:stream'
 import { LcovParser } from '../parser.js'
 
 export interface Options {
+    /**
+     * The field names to use for parsing. These will only be used if {@link Options#parser} is not present.
+     */
     fieldNames?: FieldNames
+    /**
+     * Input source to read from.
+     * This can be a `string`, [Readable](https://nodejs.org/api/stream.html#readable-streams), `Buffer`, or `ArrayBuffer`.
+     */
     from: string | Readable | Buffer | ArrayBuffer
+    /**
+     * A parser instance that should be used for parsing, instead of creating a new one internally.
+     */
     parser?: LcovParser
 }
 
 export interface StreamOptions
     extends Omit<TransformOptions, 'transform' | 'flush' | 'writableObjectMode' | 'readableObjectMode'> {
+    /**
+     * The field names to use for parsing. These will only be used if {@link StreamOptions#parser} is not present.
+     */
     fieldNames?: FieldNames
+    /**
+     * A parser instance that should be used for parsing, instead of creating a new one internally.
+     */
     parser?: LcovParser
 }
 
