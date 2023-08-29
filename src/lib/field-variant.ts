89ebffc48de1789d1a030c7b36b68017be12b9e0
Friedemann Sommer <social@friedemannsommer.com>
2023-08-29 02:16:02 +0200

    added `EmptyVariants` and `NonEmptyVariants` type aliases updated return values for `isEmptyField` and `isNonEmptyField`

--- src/lib/field-variant.ts
+++ src/lib/field-variant.ts
@@ -1,11 +1,27 @@
 import { Variant } from '../constants.js'
 
+export type EmptyVariants = Variant.EndOfRecord | Variant.None
+export type NonEmptyVariants =
+    | Variant.BranchHit
+    | Variant.BranchInstrumented
+    | Variant.BranchLocation
+    | Variant.FilePath
+    | Variant.FunctionExecution
+    | Variant.FunctionHit
+    | Variant.FunctionInstrumented
+    | Variant.FunctionLocation
+    | Variant.LineHit
+    | Variant.LineInstrumented
+    | Variant.LineLocation
+    | Variant.TestName
+    | Variant.Version
+
 /**
  * Checks if the given {@link Variant} doesn't have a value.
  *
  * @param variant - The field entry variant to check.
  */
-export function isEmptyField(variant: Variant): variant is Variant.EndOfRecord {
+export function isEmptyField(variant: Variant): variant is EmptyVariants {
     return variant === Variant.EndOfRecord || variant === Variant.None
 }
 
@@ -14,6 +30,6 @@ export function isEmptyField(variant: Variant): variant is Variant.EndOfRecord {
  *
  * @param variant - The field entry variant to check.
  */
-export function isNonEmptyField(variant: Variant): boolean {
+export function isNonEmptyField(variant: Variant): variant is NonEmptyVariants {
     return !isEmptyField(variant)
 }
