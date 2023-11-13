c02770d046ed32482314bd47bdf9b1b9a5446fd1
Friedemann Sommer <social@friedemannsommer.com>
2023-11-13 20:52:12 +0100

    updated build config to enable minifying

--- build.config.js
+++ build.config.js
@@ -15,7 +15,12 @@ export default defineBuildConfig({
     rollup: {
         emitCJS: true,
         esbuild: {
-            minify: false,
+            // reduce the overall package size by minifying the generated code.
+            // this isn't a "perfect" solution,
+            // but unless someone really wants to see the actual code
+            // (which they can by simply visiting GitHub),
+            // this will save roughly 20%.
+            minify: true,
             platform: 'neutral',
             target: 'node16',
             treeShaking: true
