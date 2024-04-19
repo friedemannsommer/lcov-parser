2c0422d651561029f3eca310ea215b807c96e2e4
Friedemann Sommer <social@friedemannsommer.com>
2024-04-20 00:21:02 +0200

    fixed naming for extensions in mocha config added `require` option to mocha config for Node.js v16

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -2,8 +2,9 @@ const nodeVersion = process.version
 const nodeMajorVersion = Number.parseInt(nodeVersion.slice(1, nodeVersion.indexOf('.')))
 
 module.exports = {
+    extensions: ['ts'],
+    loader: nodeMajorVersion <= 16 ? 'ts-node/esm' : undefined,
     'node-option': nodeMajorVersion > 16 ? ['import=./node/register.mjs'] : undefined,
-    extension: ['ts'],
-    spec: 'src/tests/**/*.spec.ts',
-    loader: nodeMajorVersion <= 16 ? 'ts-node/esm' : undefined
+    require: nodeMajorVersion <= 16 ? 'ts-node/register' : undefined,
+    spec: 'src/tests/**/*.spec.ts'
 }
